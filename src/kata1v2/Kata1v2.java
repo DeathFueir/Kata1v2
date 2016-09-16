d8c9fe38155a6d04c1d921617444dd46bccbeaf6
usuario <usuario@10.22.143.48>
2016-09-16 09:51:34 +0100

    Kata1v2 quitando comentarios.

--- src/kata1v2/Kata1v2.java
+++ src/kata1v2/Kata1v2.java
@@ -1,21 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata1v2;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-/**
- *
- * @author usuario
- */
 public class Kata1v2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Calendar date;
         date = GregorianCalendar.getInstance();
